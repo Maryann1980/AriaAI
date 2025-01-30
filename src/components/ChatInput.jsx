c19479f736ccf3aab296e1ab3641286602f59101
Maryann1980 <igwemaryanneifunanya@gmail.com>
2025-01-30 18:15:32 +0100

    new changes

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -25,7 +25,7 @@ export function ChatInput({ chatMessages, setChatMessages }) {
     setChatMessages([...newChatMessages, typingMessage]);
 
     try {
-      const response = await fetch('http://localhost:5000/generate', {
+      const response = await fetch('ariaai-backend-production.up.railway.app/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ prompt: inputText }), // Changed 'message' to 'prompt'
